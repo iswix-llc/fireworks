638105a209ef142920f4973888933f54683ff99d
Christopher Painter <chrpai@iswix.com>
2019-02-13 17:47:23 -0600

    Initial Checkin
    Fireworks used to be it's own project on CodePlex.  At some point I merged it into IsWiX proper.  Now with the availablity of NuGet and better CI-CD pipelines it is being split back out.    It is also new and refactored in WPF instead of WinForms.

---
+++ Application/FireworksFramework/Managers/FireworksManager.cs
@@ -0,0 +1,55 @@
+﻿///////////////////////////////////////////////
+// Copyright (C) 2010-2019 ISWIX, LLC
+// Web: http://www.iswix.com
+// All Rights Reserved
+///////////////////////////////////////////////
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using FireworksFramework.Views;
+
+namespace FireworksFramework.Managers
+{
+    public class FireworksManager
+    {
+        private static readonly Lazy<FireworksManager> lazy = new Lazy<FireworksManager>(() => new FireworksManager());
+        public static FireworksManager FireworksManagerInstance { get { return lazy.Value; } }
+
+        public string FilePath
+        {
+            get
+            {
+                // Empty if no parms passed, otherwise the File Path of the default document to be opened.
+                string filePath;
+                string[] args = Environment.GetCommandLineArgs();
+                if (args.Length > 1)
+                {
+                    filePath = args[1];
+                }
+                else
+                {
+                    filePath = string.Empty;
+                }
+                return filePath;
+            }
+        }
+        public BitmapImage BrandingBitMap { get; set; }
+        public string ProductName { get; set; }
+        public FireworksManager()
+        {
+            try
+            {
+                Environment.CurrentDirectory = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public void Start()
+        {
+            FireworksView fireworksView = new FireworksView();
+            fireworksView.ShowDialog();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# fireworks
+Fireworks is the core library at the heart of Industrial Strength Windows Installer XML ( IsWiX ). It makes authoring XML editor applications a breeze.
